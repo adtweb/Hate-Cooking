122daf3724702b46262b0895ee17cb4711c68348
Olga Scherbakova <o.scherbakova@inetstudio.ru>
2024-09-03 13:42:47 +0300

    add permission model

---
+++ app/Models/Permission.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Permission extends Model
+{
+    protected $fillable = ['name'];
+}
