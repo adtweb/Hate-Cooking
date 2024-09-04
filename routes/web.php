34c723ef20765f61eacf984d3f7036540c24fd22
Olga Scherbakova <o.scherbakova@inetstudio.ru>
2024-09-04 04:46:40 +0300

    add welcome page

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,6 @@
 declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
+use App\Recipe;
 
-Route::get('/', fn () => view('welcome'));
+Route::get('/', fn () => view('welcome')->withRecipes(Recipe::all));
