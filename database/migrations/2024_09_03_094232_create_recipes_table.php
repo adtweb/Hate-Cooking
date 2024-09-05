a8b4d8a8788d3a884991ec76a1cbc93e1bf2f1bf
Olga Scherbakova <o.scherbakova@inetstudio.ru>
2024-09-05 12:41:54 +0300

    fix slug seeding

--- database/migrations/2024_09_03_094232_create_recipes_table.php
+++ database/migrations/2024_09_03_094232_create_recipes_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('recipes', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('value');
-            $table->string('slug');
+            $table->string('slug')->unique();
             $table->foreignUuid('user_id')->constrained('users')->cascadeOnDelete()->index();
             $table->string('photo_url');
             $table->timestamps();
