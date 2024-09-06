9692ebbc6667bbf0cd50b1031b879942adff540e
Olga Scherbakova <o.scherbakova@inetstudio.ru>
2024-09-06 16:56:01 +0300

    add primary to foreign key

--- database/migrations/2024_09_05_211454_create_comments_table.php
+++ database/migrations/2024_09_05_211454_create_comments_table.php
@@ -12,9 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('comments', function (Blueprint $table) {
-            $table->uuid('id');
-            $table->foreignUuid('user_id')->index()->cascadeOnDelete();
-            $table->foreignUuid('recipe_id')->index()->cascadeOnDelete();
+            $table->uuid('id')->primary();
+            $table->foreignUuid('user_id')->cascadeOnDelete();
+            $table->foreignUuid('recipe_id')->cascadeOnDelete();
             $table->text('comment');
             $table->timestamps();
         });
