fe812c887514830bf82863da1f68fa9c208dccc9
Olga Scherbakova <o.scherbakova@inetstudio.ru>
2024-09-07 09:34:42 +0300

    fix warnings

--- app/Filament/Resources/RecipeResource.php
+++ app/Filament/Resources/RecipeResource.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\RecipeResource\Pages;
-use App\Filament\Resources\RecipeResource\RelationManagers;
 use App\Models\Recipe;
 use App\Models\User;
 use Filament\Forms;
@@ -12,8 +11,6 @@ use Filament\Forms\Set;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Illuminate\Support\Str;
 
 class RecipeResource extends Resource
