753c875027af83b425ab352aacee66c5655cbb83
Olga Scherbakova <o.scherbakova@inetstudio.ru>
2024-09-07 09:35:47 +0300

    fix warnings

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,7 +7,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use LaravelJsonApi\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Notifications\Notifiable;
 
 class Category extends Model
