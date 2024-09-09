3944a2403ec2abaaf3382911a13ca9b7c38fe0a6
Olga Scherbakova <o.scherbakova@inetstudio.ru>
2024-09-09 15:21:57 +0300

    fix welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,28 +20,19 @@
                         </div>
                         @if (Route::has('login'))
                             <nav class="-mx-3 flex flex-1 justify-end">
+                                <x-responsive-nav-link :href="route('recipes')" :active="request()->routeIs('recipes')">
+                                    Рекомендуем
+                                </x-responsive-nav-link>
                                 @auth
-                                    <a
-                                        href="{{ url('/recipes') }}"
-                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                                    >
-                                        Рекомендуем
-                                    </a>
                                 @else
-                                    <a
-                                        href="{{ route('login') }}"
-                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                                    >
+                                    <x-responsive-nav-link :href="route('login')" :active="request()->routeIs('login')">
                                         Войти
-                                    </a>
+                                    </x-responsive-nav-link>
 
                                     @if (Route::has('register'))
-                                        <a
-                                            href="{{ route('register') }}"
-                                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                                        >
+                                        <x-responsive-nav-link :href="route('register')" :active="request()->routeIs('register')">
                                             Регистрация
-                                        </a>
+                                        </x-responsive-nav-link>
                                     @endif
                                 @endauth
                             </nav>
