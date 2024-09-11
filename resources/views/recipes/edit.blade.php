224eb34971b35b32873c4df980b1095f0952e226
Olga Scherbakova <o.scherbakova@inetstudio.ru>
2024-09-11 17:05:58 +0300

    fix model update

--- resources/views/recipes/edit.blade.php
+++ resources/views/recipes/edit.blade.php
@@ -15,19 +15,20 @@
 
                         <div>
                             <x-input-label for="value" :value="__('Название')" />
-                            <x-text-input id="value" name="value" type="text" class="mt-1 block w-full" :value="old('value'), $recipe->value" required autofocus autocomplete="value" />
+                            <x-text-input id="value" name="value" type="text" class="mt-1 block w-full" :value="old('value') or $recipe->value" required autofocus autocomplete="value" />
                             <x-input-error class="mt-2" :messages="$errors->get('value')" />
                         </div>
 
                         <div>
                             <x-input-label for="photo_url" :value="__('Фотография')" />
+                            <img src="/storage/{{ $recipe->photo_url }}" alt="{{ $recipe->value }}" />
                             <x-text-input id="photo_url" name="photo_url" type="file" class="mt-1 block w-full" required autofocus />
                             <x-input-error class="mt-2" :messages="$errors->get('photo_url')" />
                         </div>
 
                         <div>
                             <x-input-label for="description" :value="__('Описание')" />
-                            <textarea name="description" id="description" cols="30" rows="10">{{ old('description'), $recipe->description }}</textarea>
+                            <textarea name="description" id="description" cols="30" rows="10">{{ old('description') or $recipe->description }}</textarea>
                             <x-input-error class="mt-2" :messages="$errors->get('description')" />
                         </div>
                         <div>
@@ -35,7 +36,7 @@
                             @foreach(\App\Models\Category::all() as $category)
                                 <div class="inline-flex p-5">
                                     <label>
-                                        <input type="checkbox" value="{{ $category->id }}" class="mt-1" name="categories[]" @checked(old('categories[]', $recipe->caregories()->find($category->id) == 'on')) /> {{ $category->value }}
+                                        <input type="checkbox" value="{{ $category->id }}" class="mt-1" name="categories[]" @checked(old('categories[$category->id]') or in_array($category->id, $categories)) /> {{ $category->value }}
                                     </label>
                                 </div>
                             @endforeach
@@ -45,13 +46,36 @@
                             @foreach(\App\Models\Quality::all() as $quality)
                                 <div class="inline-flex p-5">
                                     <label>
-                                        <input type="checkbox" value="{{ $quality->id }}" class="mt-1" name="qualities[]" @checked(old('qualities[]', $recipe->caregories()->find($category->id) == 'on')) /> {{ $quality->value }}
+                                        <input type="checkbox" value="{{ $quality->id }}" class="mt-1" name="qualities[]" @checked(old('qualities[$quality->id]') or in_array($quality->id, $qualities)) /> {{ $quality->value }}
                                     </label> {{ $quality->value }}
                                 </div>
                             @endforeach
                         </div>
                         <x-primary-button type="submit">Сохранить</x-primary-button>
                     </form>
+                    <div class="mt-12">
+                        <h2 id="ingredients" class="text-2xl font-semibold">Ингредиенты</h2>
+                        @foreach($ingredients as $ingredient)
+                            <div class="row">
+                                <div class="col bg-light border-bottom">{{ $ingredient->value }}</div>
+                                <div class="col bg-light border-bottom bold">{{ $ingredient->quantity }}</div>
+                            </div>
+                        @endforeach
+                    </div>
+
+                    <div class="mt-12">
+                        <h2 id="ingredients" class="text-2xl font-semibold">Приготовление</h2>
+                        @foreach($steps as $step)
+                            <div class="row">
+                                <div class="col bg-light border-bottom">
+                                    <img src="/storage/{{ $step->photo_url }}" alt="{{ $loop->iteration }}" />
+                                </div>
+                                <div class="col bg-light border-bottom">
+                                    {!! $step->html !!}
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
                 </div>
             </div>
         </div>
