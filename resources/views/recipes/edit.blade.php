e32497a839e00582d341503622969097f276b72d
Olga Scherbakova <o.scherbakova@inetstudio.ru>
2024-09-11 18:05:02 +0300

    fix model update

--- resources/views/recipes/edit.blade.php
+++ resources/views/recipes/edit.blade.php
@@ -22,7 +22,7 @@
                         <div>
                             <x-input-label for="photo_url" :value="__('Фотография')" />
                             <img src="/storage/{{ $recipe->photo_url }}" alt="{{ $recipe->value }}" />
-                            <x-text-input id="photo_url" name="photo_url" type="file" class="mt-1 block w-full" value="{{ $recipe->photo_url }}" />
+                            <x-text-input id="photo_url" name="photo_url" type="file" class="mt-1 block w-full" />
                             <x-input-error class="mt-2" :messages="$errors->get('photo_url')" />
                         </div>
 
@@ -55,16 +55,59 @@
                     </form>
                     <div class="mt-12">
                         <h2 id="ingredients" class="text-2xl font-semibold">Ингредиенты</h2>
+                        @if(empty($steps))
+                            <p>Ингредиенты не добавлены</p>
+                        @endif
+                            <form action="{{ route('recipes.ingredients.store', $recipe) }}" method="POST" class="mt-2">
+                                @csrf
+
+                                <div>
+                                    <x-input-label for="value" :value="__('Продукт')" />
+                                    <x-text-input id="value" name="value" type="text" class="mt-1 block w-full" :value="old('value')" required autofocus autocomplete="value" />
+                                    <x-input-error class="mt-2" :messages="$errors->get('value')" />
+                                </div>
+                                <div>
+                                    <x-input-label for="value" :value="__('Количество')" />
+                                    <x-text-input id="quantity" name="quantity" type="text" class="mt-1 block w-full" :value="old('value')" required autofocus autocomplete="quantity" />
+                                    <x-input-error class="mt-2" :messages="$errors->get('quantity')" />
+                                </div>
+                                <x-primary-button type="submit">Добавить ингредиент</x-primary-button>
+                            </form>
                         @foreach($ingredients as $ingredient)
                             <div class="row">
                                 <div class="col bg-light border-bottom">{{ $ingredient->value }}</div>
                                 <div class="col bg-light border-bottom bold">{{ $ingredient->quantity }}</div>
+                                <div class="col bg-light border-bottom bold">
+                                    <form
+                                        action="{{ route('recipes.ingredients.destroy', ['recipe' => $recipe, 'ingredient' => $ingredient]) }}"
+                                        method="POST" class="mt-2">
+                                        @csrf
+                                        @method('DELETE')
+
+                                        <x-danger-button type="submit">Удалить</x-danger-button>
+                                    </form>
+
+                                </div>
                             </div>
                         @endforeach
                     </div>
 
                     <div class="mt-12">
                         <h2 id="ingredients" class="text-2xl font-semibold">Приготовление</h2>
+                        @if(empty($steps))
+                            <p>Способ приготовления не добавлен</p>
+                        @endif
+                        <form action="{{ route('recipes.steps.store', $recipe) }}" method="POST" class="mt-2">
+                            @csrf
+
+                            <x-input-label for="photo_url" :value="__('Фотография')" />
+                            <x-text-input id="photo_url" name="photo_url" type="file" class="mt-1 block w-full" />
+                            <x-input-error class="mt-2" :messages="$errors->get('photo_url')" />
+                            <textarea name="description" id="description" cols="30" rows="5" class="w-full">
+                                {{ old('description') }}
+                            </textarea>
+                            <x-primary-button type="submit">Добавить ингредиент</x-primary-button>
+                        </form>
                         @foreach($steps as $step)
                             <div class="row">
                                 <div class="col bg-light border-bottom">
@@ -72,6 +115,17 @@
                                 </div>
                                 <div class="col bg-light border-bottom">
                                     {!! $step->html !!}
+                                </div>
+                                <div class="col bg-light border-bottom bold">
+                                    <form
+                                        action="{{ route('recipes.steps.destroy', ['recipe' => $recipe, 'step' => $step]) }}"
+                                        method="POST" class="mt-2">
+                                        @csrf
+                                        @method('DELETE')
+
+                                        <x-danger-button type="submit">Удалить</x-danger-button>
+                                    </form>
+
                                 </div>
                             </div>
                         @endforeach
