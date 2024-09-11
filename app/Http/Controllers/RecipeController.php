749bfe8f9ac82644819183b3d3db39bf4f23e919
Olga Scherbakova <o.scherbakova@inetstudio.ru>
2024-09-11 15:48:09 +0300

    fix model creating

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -47,7 +47,7 @@ class RecipeController extends Controller
         $recipe->categories()->attach($request->categories);
         $recipe->qualities()->attach($request->qualities);
 
-        return to_route('recipes.edit', $recipe);
+        return to_route('recipes.edit', ['recipe' => $recipe]);
     }
 
     /**
