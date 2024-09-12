9ff16d355ae2d4afb539e4fc5f37006d463d8999
Olga Scherbakova <o.scherbakova@inetstudio.ru>
2024-09-12 10:07:43 +0300

    fix layout

--- resources/views/recipes/show.blade.php
+++ resources/views/recipes/show.blade.php
@@ -1,10 +1,12 @@
 <x-app-layout>
     <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Рецепты') }} &gt;  {{ $recipe->value }}
+            <a href="{{ route('recipes.index') }}">{{ __('Рецепты') }}</a> &gt;  {{ $recipe->value }}
         </h2>
     </x-slot>
+        </div>
     </div>
 
     <div class="py-12">
